33e2304b8123723a5dc3bfcd79a42573bef41bc4
Affonso Ruiz <affonsohenriqueruiz@gmail.com>
2023-01-01 08:53:52 -0400

    feat: adding sigup route logic and update models

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Post } from '@nestjs/common';
+import { Body } from '@nestjs/common/decorators';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  signup() {
-    return this.authService.signin();
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
   }
 
   @Post('/signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { ForbiddenException } from '@nestjs/common/exceptions';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return { msg: 'I am sign up' };
+
+  async signup(data: AuthDto) {
+    const hash = await argon.hash(data.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: data.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+
+        throw error;
+      }
+    }
   }
 
   signin() {
